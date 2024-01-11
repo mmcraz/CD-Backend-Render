63f94f2e95c384b4c107e238d9ababdd435a6b1e
mmcraz <156281914+mmcraz@users.noreply.github.com>
2024-01-11 11:18:39 +0530

    First Push

---
+++ index.js
@@ -0,0 +1,52 @@
+var Express = require("express");
+var MongoClient = require("mongodb").MongoClient;
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+
+var app = Express();
+app.use(cors());
+
+var DATABASENAME = "tododb";
+var database;
+
+const cs =
+  "mongodb+srv://admin:admin@cluster0.lmpvayp.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5039;
+app.listen(PORT, () => {
+  MongoClient.connect(
+    process.env.MONGODB_CONNECT_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+      database = client.db(DATABASENAME);
+      console.log("Mongo DB Connection successful");
+    }
+  );
+});
+
+app.get("/api/tododb/getData/", (request, response) => {
+  database
+    .collection("tododbCollection")
+    .find({})
+    .toArray((error, result) => {
+      response.send(result);
+    });
+});
+
+app.post("/api/tododb/addData/", multer().none(), (request, response) => {
+  database
+    .collection("tododbCollection")
+    .count({}, function (error, numOfDocs) {
+      database.collection("tododbCollection").insertOne({
+        id: (numOfDocs + 1).toString(),
+        desc: request.body.addData,
+      });
+      response.json("Added Succefully");
+    });
+});
+
+app.delete("/api/tododb/deleteData/", (request, response) => {
+  database.collection("tododbCollection").deleteOne({ id: request.query.id });
+  response.json("Deleted Succefully");
+});
